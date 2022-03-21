82e8b8c6272785edfa6c8f2ab616f1ba0710ff30
Aditya Anand <adityash212@gmail.com>
2022-03-21 14:22:28 +0530

    Update urls.py

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -20,7 +20,7 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('form.urls')),
+    path('', include('form.urls')),
     path('auth/', views.obtain_auth_token,name="obtain-token"),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
